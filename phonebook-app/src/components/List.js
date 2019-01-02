e205d96780e3fd2f607999ebd3041e3bc9df8f4c
Mukerrem Akkoyun <mukerrem.akkoyun@nmqdigital.com>
2019-01-02 23:20:44 +0300

    List component created

--- phonebook-app/src/components/List.js
+++ phonebook-app/src/components/List.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
+import './List.css'
 
 export default class List extends Component {
 
   render() {
     return (
-      <div>
-        <input name="filter" id="filter" placeholder="Filter by name or phone"></input>
+      <div className={"listArea"}>
+        <input name={"filter"} id={"filter"} placeholder={"Filter by name or phone"}></input>
+        <ul className={"list"}>
+            <li></li>
+            <li>aaaa</li>
+            <li>tttt</li>
+            <li>ssss</li>
+            <li>aaaa</li>
+        </ul>
       </div>
     )
   }
 }
+
