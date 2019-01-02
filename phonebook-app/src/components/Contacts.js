05a9a0ce4944e7fe2280660e4da7a5e39ba7b5d1
Mukerrem Akkoyun <mukerrem.akkoyun@nmqdigital.com>
2019-01-02 22:57:24 +0300

    component created

---
+++ phonebook-app/src/components/Contacts.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+export default class Contacts extends Component {
+
+    state = {
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                hey
+            </div>
+        )
+    }
+}
