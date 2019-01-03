9f458bc182e94b17090f1378948a0089db069f0e
Mukerrem Akkoyun <mukerrem.akkoyun@nmqdigital.com>
2019-01-03 11:05:43 +0300

    implemented on change evennt

--- phonebook-app/src/components/Form.js
+++ phonebook-app/src/components/Form.js
@@ -1,17 +1,44 @@
 import React, {Component} from 'react';
 
 class Form extends Component {
-	state = {
 
+	constructor(props) {
+	  super()
+	
+	  this.onChange = this.onChange.bind(this);
+	};
+	
+	state = {
+		name: '',
+		phone: ''
+	};
+
+	onChange(e){
+		this.setState({
+			[e.target.name]:e.target.value
+		})
+		console.log("s");
 	};
 
 	render() {
 		return (
 			<div>
 				<form>
-					<input name="name" id="name" placeholder="Enter a name"/>
+					<input 
+					name="name" 
+					id="name" 
+					value={this.state.name} 
+					onChange={this.onChange} p
+					laceholder="Enter a name"/>
+
 					<br/>
-					<input name="phone" id="phone" placeholder="Enter a phone"/>
+
+					<input 
+					name="phone" 
+					id="phone" 
+					value={this.state.phone} 
+					onChange={this.onChange} 
+					placeholder="Enter a phone"/>
 					<button>Add</button>
 				</form>
 			</div>
