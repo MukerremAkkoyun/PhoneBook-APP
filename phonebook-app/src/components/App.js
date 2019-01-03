c310486190326a7f727f8b5651c097cec9818398
Mukerrem Akkoyun <mukerrem.akkoyun@nmqdigital.com>
2019-01-03 11:05:15 +0300

    created state

--- phonebook-app/src/components/App.js
+++ phonebook-app/src/components/App.js
@@ -4,10 +4,25 @@ import '../App.css';
 import Contacts from "./Contacts";
 
 class App extends Component {
+
+  state = {
+    array: [{
+        name : 'mehmet',
+        phone: '456 32 22'
+    },{
+        name : 'eleman2',
+        phone: 'phone2'
+    },{
+        name : 'eleman3',
+        phone: 'phone3'
+    }]
+};
+
+
   render() {
     return (
       <div className="App">
-        <Contacts />
+        <Contacts  contacts={this.state.array} />
       </div>
     );
   }
