de4cfdd0f27981fbb82b72a73da7e15c5155f805
Mukerrem Akkoyun <mukerrem.akkoyun@nmqdigital.com>
2019-01-03 11:05:59 +0300

    created dynamic list

--- phonebook-app/src/components/List.js
+++ phonebook-app/src/components/List.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import './List.css'
 
+
+
 export default class List extends Component {
+
+  static propTypes = {
+		contacts: PropTypes.array.isRequired,
+	};
 
   render() {
     return (
       <div className={"listArea"}>
         <input name={"filter"} id={"filter"} placeholder={"Filter by name or phone"}></input>
         <ul className={"list"}>
-            <li></li>
-            <li>aaaa</li>
-            <li>tttt</li>
-            <li>ssss</li>
-            <li>aaaa</li>
+        {
+						this.props.contacts.map(a =>
+							<li key={a.phone}>
+								<span className={"name"}>{a.name}</span>
+								<span className={"phone"}>{a.phone}</span>
+								<span className={"clearfix"}></span>
+							</li>
+						)
+					}
         </ul>
       </div>
     )
